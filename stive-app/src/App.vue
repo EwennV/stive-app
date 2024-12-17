dcb7dfd3e297bc8b06e57401d6b4effc40c55020
barthelemy-lebel <barthelemylebel@orange.fr>
2024-12-17 14:49:44 +0100

    Ajout d'une page homme contenant la sidebar et la topbar

--- stive-app/src/App.vue
+++ stive-app/src/App.vue
@@ -1,12 +1,8 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
-import TheWelcome from './components/TheWelcome.vue';
-import Sidebar from './components/Sidebar.vue'
 </script>
 
 <template>
   <main>
-    <Sidebar />
   </main>
 </template>
 
